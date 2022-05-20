b9c582d34b43ffa0ebe4ef45d0ae15b1a5c80fda
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-05-20 19:15:43 +0200

    Update en_US

--- en_US.php
+++ en_US.php
@@ -1,5 +1,28 @@
 <?php
-    $langArray = array(
+    $indexLangArray = array(
         "navbar-home" => "Home",
+        "navbar-login" => "Login",
+        "selectlang-selectlanguage" => "Select language",
+        "selectlang-submit" => "Submit",
+        "welcome-lenncord" => " Welcome to the official LennCord website!",
+        "welcome-lenncord-text" => "Chat with your friends and your family.",
+        "contact-title" => "Contact",
+        "contact-send" => "The contact email was sent!",
+        "contact-name-placeholder" => "Your name",
+        "contact-username-placeholder" => "Your username on LennCord",
+        "contact-userid-placeholder" => "Your userid on LennCord",
+        "contact-email-placeholder" => "Your email",
+        "contact-priority-high" => "High",
+        "contact-priority-middle" => "Middle",
+        "contact-priority-low" => "Low",
+        "contact-button-send" => "Send",
+        "features-guilds-title" => "Guilds",
+        "features-guilds-description" => "We offer you guilds, you can create guilds and join guilds. You can chat and talk on guilds with your friends and family.",
     );
+    $registerLangArray = array(
+        "filter_validate_email_error" => "Please enter a valid email.<br>",
+        "filter_username_error" => "Please enter an username.",
+        "filter_password_error" => "Please enter a password.",
+        "filter_repeat_password_error" => "Please repeat the password.",
+    )
 ?>
